7bbad4266ff7972d74ef558d11299b3bfdeb1e89
Dominic <dominic.heun@gmx.net>
2018-03-21 17:56:31 +0100

    Moved to IntelliJ Window builder

---
+++ src/me/thedome/util/MainFrame.java
@@ -0,0 +1,18 @@
+package me.thedome.util;
+
+import javax.swing.*;
+
+/**
+ * Package me.thedome.util
+ * Created on 03/2018.
+ */
+
+public class MainFrame {
+	private JPanel panel1;
+	private JButton stopButton;
+	private JEditorPane editorPane1;
+	private JButton pasteButton;
+	private JSpinner spinner1;
+	private JButton pasteFromClipbordButton;
+	private JSpinner spinner2;
+}
